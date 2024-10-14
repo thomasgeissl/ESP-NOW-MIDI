2669638d71164820bb33913132fe2f021987a189
Thomas Geissl <thomas.geissl@gmail.com>
2024-10-14 15:26:28 +0200

    updated example

--- examples/sender/sender.ino
+++ examples/sender/sender.ino
@@ -3,29 +3,37 @@
 #include "esp_now_midi.h"
 
 
-// REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+// run the print_mac firmware and adjust the mac address
+uint8_t broadcastAddress[6] = { 0xCC, 0x8D, 0xA2, 0x8B, 0x85, 0x1C };
 
 esp_now_midi ESP_NOW_MIDI;
 void customOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("Custom Callback - Status: ");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Failure");
 }
- 
+
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   ESP_NOW_MIDI.setup(broadcastAddress, customOnDataSent);
 }
- 
+
 void loop() {
-  esp_err_t result = ESP_NOW_MIDI.sendNoteOn(60,127,1);
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
+  esp_err_t result = ESP_NOW_MIDI.sendNoteOn(60, 127, 1);
+
+  if (result != ESP_OK) {
     Serial.println("Error sending the data");
   }
+  delay(100);
+  result = ESP_NOW_MIDI.sendNoteOff(60, 127, 0);
+  delay(100);
+  result = ESP_NOW_MIDI.sendControlChange(1, 127, 1);
+  delay(100);
+  result = ESP_NOW_MIDI.sendControlChange(1, 0, 1);
+  delay(100);
+  result = ESP_NOW_MIDI.sendPitchBend(16383, 1);
+  delay(100);
+  result = ESP_NOW_MIDI.sendPitchBend(0, 1);
+
   delay(2000);
 }
