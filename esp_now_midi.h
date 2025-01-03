c075a34d592ff688da809f6594dceec7c5ea9ce8
Thomas Geissl <thomas.geissl@gmail.com>
2025-01-03 16:14:28 +0100

    added midi handlers for the sender

--- esp_now_midi.h
+++ esp_now_midi.h
@@ -2,22 +2,28 @@
 #include <esp_now.h>
 #include "./midiHelpers.h"
 
-class esp_now_midi {
+class esp_now_midi
+{
 public:
   // Typedef for the callback function signature
   typedef void (*DataSentCallback)(const uint8_t *mac_addr, esp_now_send_status_t status);
 
+  // typedef void (*MidiCallback)(byte channel, byte firstByte, byte secondByte);
+
   // callback when data is sent (must be static for esp_now_register_send_cb)
-  static void DefaultOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  static void DefaultOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+  {
     Serial.print("\r\nLast Packet Send Status:\t");
     Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
   }
 
-  void setup(const uint8_t broadcastAddress[6], DataSentCallback callback = DefaultOnDataSent) {
+  void setup(const uint8_t broadcastAddress[6], DataSentCallback callback = DefaultOnDataSent)
+  {
     // Copy the broadcast address into the class member variable
     memcpy(_broadcastAddress, broadcastAddress, sizeof(_broadcastAddress));
 
-    if (esp_now_init() != ESP_OK) {
+    if (esp_now_init() != ESP_OK)
+    {
       Serial.println("Error initializing ESP-NOW");
       return;
     }
@@ -31,13 +37,15 @@ public:
     _peerInfo.encrypt = false;
 
     // Add peer
-    if (esp_now_add_peer(&_peerInfo) != ESP_OK) {
+    if (esp_now_add_peer(&_peerInfo) != ESP_OK)
+    {
       Serial.println("Failed to add peer");
       return;
     }
   }
 
-  esp_err_t sendNoteOn(byte note, byte velocity, byte channel) {
+  esp_err_t sendNoteOn(byte note, byte velocity, byte channel)
+  {
     midi_message message;
     message.channel = channel;
     message.status = MIDI_NOTE_ON;
@@ -46,7 +54,8 @@ public:
     return esp_now_send(_broadcastAddress, (uint8_t *)&message, sizeof(message));
   }
 
-  esp_err_t sendNoteOff(byte note, byte velocity, byte channel) {
+  esp_err_t sendNoteOff(byte note, byte velocity, byte channel)
+  {
     midi_message message;
     message.channel = channel;
     message.status = MIDI_NOTE_OFF;
@@ -55,7 +64,8 @@ public:
     return esp_now_send(_broadcastAddress, (uint8_t *)&message, sizeof(message));
   }
 
-  esp_err_t sendControlChange(byte control, byte value, byte channel) {
+  esp_err_t sendControlChange(byte control, byte value, byte channel)
+  {
     midi_message message;
     message.channel = channel;
     message.status = MIDI_CONTROL_CHANGE;
@@ -64,7 +74,8 @@ public:
     return esp_now_send(_broadcastAddress, (uint8_t *)&message, sizeof(message));
   }
 
-  esp_err_t sendProgramChange(byte program, byte channel) {
+  esp_err_t sendProgramChange(byte program, byte channel)
+  {
     midi_message message;
     message.channel = channel;
     message.status = MIDI_PROGRAM_CHANGE;
@@ -72,7 +83,8 @@ public:
     return esp_now_send(_broadcastAddress, (uint8_t *)&message, sizeof(message));
   }
 
-  esp_err_t sendAfterTouch(byte pressure, byte channel) {
+  esp_err_t sendAfterTouch(byte pressure, byte channel)
+  {
     midi_message message;
     message.channel = channel;
     message.status = MIDI_AFTERTOUCH;
@@ -80,7 +92,8 @@ public:
     return esp_now_send(_broadcastAddress, (uint8_t *)&message, sizeof(message));
   }
 
-  esp_err_t sendAfterTouch(byte note, byte pressure, byte channel) {
+  esp_err_t sendAfterTouch(byte note, byte pressure, byte channel)
+  {
     midi_message message;
     message.channel = channel;
     message.status = MIDI_POLY_AFTERTOUCH;
@@ -89,26 +102,105 @@ public:
     return esp_now_send(_broadcastAddress, (uint8_t *)&message, sizeof(message));
   }
   // for those using the teensy library
-  esp_err_t sendAfterTouchPoly(byte note, byte pressure, byte channel) {
+  esp_err_t sendAfterTouchPoly(byte note, byte pressure, byte channel)
+  {
     return sendAfterTouch(note, pressure, channel);
   }
 
-  esp_err_t sendPitchBend(uint16_t value, byte channel) {
+  esp_err_t sendPitchBend(uint16_t value, byte channel)
+  {
     midi_message message;
     message.channel = channel;
     message.status = MIDI_PITCH_BEND;
-    
+
     // Ensure value is within the valid 14-bit range (0 - 16383)
     value = value & 0x3FFF; // Mask to ensure it's 14 bits (0x3FFF = 16383 in decimal)
 
     // Split the 14-bit value into LSB and MSB (each 7 bits)
-    message.firstByte = value & 0x7F;        // LSB: lower 7 bits of the pitch bend value
+    message.firstByte = value & 0x7F;         // LSB: lower 7 bits of the pitch bend value
     message.secondByte = (value >> 7) & 0x7F; // MSB: upper 7 bits of the pitch bend value
 
     return esp_now_send(_broadcastAddress, (uint8_t *)&message, sizeof(message));
+  }
+
+  void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+  {
+    midi_message message;
+    memcpy(&message, incomingData, sizeof(message));
+
+    auto status = message.status;
+    auto channel = message.channel;
+
+    switch (message.status)
+    {
+    case MIDI_NOTE_ON:
+      if (onNoteOnHandler)
+        onNoteOnHandler(message.channel, message.firstByte, message.secondByte);
+      break;
+    case MIDI_NOTE_OFF:
+      if (onNoteOffHandler)
+        onNoteOffHandler(message.channel, message.firstByte, message.secondByte);
+      break;
+    case MIDI_CONTROL_CHANGE:
+      if (onControlChangeHandler)
+        onControlChangeHandler(message.channel, message.firstByte, message.secondByte);
+      break;
+    case MIDI_PROGRAM_CHANGE:
+      if (onProgramChangeHandler)
+        onProgramChangeHandler(message.channel, message.firstByte);
+      break;
+    case MIDI_AFTERTOUCH:
+      if (onAfterTouchChannelHandler)
+        onAfterTouchChannelHandler(message.channel, message.firstByte);
+      break;
+    case MIDI_POLY_AFTERTOUCH:
+      if (onAfterTouchPolyHandler)
+        onAfterTouchPolyHandler(message.channel, message.firstByte, message.secondByte);
+      break;
+    case MIDI_PITCH_BEND:
+      int pitchBendValue = (message.secondByte << 7) | message.firstByte;
+      if (onPitchBendHandler)
+        onPitchBendHandler(message.channel, pitchBendValue);
+      break;
+    }
+  }
+  void setHandleNoteOn(void (*callback)(byte channel, byte note, byte velocity)) { 
+  onNoteOnHandler = callback; 
 }
 
+void setHandleNoteOff(void (*callback)(byte channel, byte note, byte velocity)) { 
+  onNoteOffHandler = callback; 
+}
+
+void setHandleControlChange(void (*callback)(byte channel, byte control, byte value)) { 
+  onControlChangeHandler = callback; 
+}
+
+void setHandleProgramChange(void (*callback)(byte channel, byte program)) { 
+  onProgramChangeHandler = callback; 
+}
+
+void setHandlePitchBend(void (*callback)(byte channel, int value)) { 
+  onPitchBendHandler = callback; 
+}
+
+void setHandleAfterTouchChannel(void (*callback)(byte channel, byte pressure)) { 
+  onAfterTouchChannelHandler = callback; 
+}
+
+void setHandleAfterTouchPoly(void (*callback)(byte channel, byte note, byte pressure)) { 
+  onAfterTouchPolyHandler = callback; 
+}
 private:
   uint8_t _broadcastAddress[6];
   esp_now_peer_info_t _peerInfo;
+
+  // MIDI Handlers
+  void (*onNoteOnHandler)(byte channel, byte note, byte velocity) = nullptr;
+  void (*onNoteOffHandler)(byte channel, byte note, byte velocity) = nullptr;
+  void (*onControlChangeHandler)(byte channel, byte control, byte value) = nullptr;
+  void (*onProgramChangeHandler)(byte channel, byte program) = nullptr;
+  void (*onPitchBendHandler)(byte channel, int value) = nullptr;
+  void (*onAfterTouchChannelHandler)(byte channel, byte value) = nullptr;
+  void (*onAfterTouchPolyHandler)(byte channel, byte note, byte value) = nullptr;
 };
--- examples/sender/sender.ino
+++ examples/sender/sender.ino
@@ -12,10 +12,47 @@ void customOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Failure");
 }
 
+void onNoteOn(byte channel, byte note, byte velocity) {
+  Serial.printf("Note On - Channel: %d, Note: %d, Velocity: %d\n", channel, note, velocity);
+}
+
+void onNoteOff(byte channel, byte note, byte velocity) {
+  Serial.printf("Note Off - Channel: %d, Note: %d, Velocity: %d\n", channel, note, velocity);
+}
+
+void onControlChange(byte channel, byte control, byte value) {
+  Serial.printf("Control Change - Channel: %d, Control: %d, Value: %d\n", channel, control, value);
+}
+
+void onProgramChange(byte channel, byte program) {
+  Serial.printf("Program Change - Channel: %d, Program: %d\n", channel, program);
+}
+
+void onPitchBend(byte channel, int value) {
+  Serial.printf("Pitch Bend - Channel: %d, Value: %d\n", channel, value);
+}
+void onAfterTouch(byte channel, byte value) {
+  Serial.printf("After Touch - Channel: %d, Value: %d\n", channel, value);
+}
+void onPolyAfterTouch(byte channel, byte note, byte value) {
+  Serial.printf("Poly After Touch - Channel: %d, note: %d, Value: %d\n", channel, note, value);
+}
+
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   ESP_NOW_MIDI.setup(broadcastAddress, customOnDataSent);
+  // ESP_NOW_MIDI.setup(broadcastAddress); //or get rid of the custom send function and use the default one
+
+  // all of these midi handlers are optional, depends on the usecase, very often you just wanna send data and not receive
+  // e.g. this can be used for calibration, or maybe you wanna connect an amp via i2s and render some sound
+  ESP_NOW_MIDI.setHandleNoteOn(onNoteOn);
+  ESP_NOW_MIDI.setHandleNoteOff(onNoteOff);
+  ESP_NOW_MIDI.setHandleControlChange(onControlChange);
+  ESP_NOW_MIDI.setHandleProgramChange(onProgramChange);
+  ESP_NOW_MIDI.setHandlePitchBend(onPitchBend);
+  ESP_NOW_MIDI.setHandleAfterTouchChannel(onAfterTouch);
+  ESP_NOW_MIDI.setHandleAfterTouchPoly(onPolyAfterTouch);
 }
 
 void loop() {
