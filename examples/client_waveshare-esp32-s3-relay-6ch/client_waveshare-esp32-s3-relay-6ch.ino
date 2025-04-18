cdb20f36932cbf4ac432492644b315b310287dc9
Thomas Geissl <thomas.geissl@gmail.com>
2025-04-19 01:26:15 +0200

    added relay controller client example

---
+++ examples/client_waveshare-esp32-s3-relay-6ch/client_waveshare-esp32-s3-relay-6ch.ino
@@ -0,0 +1,75 @@
+#include "config.h"
+#include <esp_now.h>
+#include <WiFi.h>
+#include "esp_now_midi.h"
+
+// on the dongle: run the print_mac firmware and paste it here
+uint8_t peerMacAddress[6] = { 0xCC, 0x8D, 0xA2, 0x8B, 0x85, 0x1C };
+
+esp_now_midi ESP_NOW_MIDI;
+
+
+
+
+void customOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  // Serial.print("Custom Callback - Status: ");
+  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Failure");
+}
+
+void onNoteOn(byte channel, byte note, byte velocity) {
+  Serial.printf("Note On - Channel: %d, Note: %d, Velocity: %d\n", channel, note, velocity);
+  if (note >= 60 && note <= 65) {
+    digitalWrite(note - 60, HIGH);
+  }
+}
+
+void onNoteOff(byte channel, byte note, byte velocity) {
+  Serial.printf("Note Off - Channel: %d, Note: %d, Velocity: %d\n", channel, note, velocity);
+  if (note >= 60 && note <= 65) {
+    digitalWrite(note - 60, LOW);
+  }
+}
+
+void onControlChange(byte channel, byte control, byte value) {
+  Serial.printf("Control Change - Channel: %d, Control: %d, Value: %d\n", channel, control, value);
+}
+
+void onProgramChange(byte channel, byte program) {
+  Serial.printf("Program Change - Channel: %d, Program: %d\n", channel, program);
+}
+
+void onPitchBend(byte channel, int value) {
+  Serial.printf("Pitch Bend - Channel: %d, Value: %d\n", channel, value);
+}
+void onAfterTouch(byte channel, byte value) {
+  Serial.printf("After Touch - Channel: %d, Value: %d\n", channel, value);
+}
+void onPolyAfterTouch(byte channel, byte note, byte value) {
+  Serial.printf("Poly After Touch - Channel: %d, note: %d, Value: %d\n", channel, note, value);
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  for (auto i = 0; i < 6; i++) {
+    pinMode(_relayPins[i], OUTPUT);
+  }
+
+  WiFi.mode(WIFI_STA);
+  ESP_NOW_MIDI.setup(peerMacAddress, customOnDataSent);
+  // ESP_NOW_MIDI.setup(peerMacAddress); //or get rid of the custom send function and use the default one
+
+  ESP_NOW_MIDI.setHandleNoteOn(onNoteOn);
+  ESP_NOW_MIDI.setHandleNoteOff(onNoteOff);
+  ESP_NOW_MIDI.setHandleControlChange(onControlChange);
+  ESP_NOW_MIDI.setHandleProgramChange(onProgramChange);
+  ESP_NOW_MIDI.setHandlePitchBend(onPitchBend);
+  ESP_NOW_MIDI.setHandleAfterTouchChannel(onAfterTouch);
+  ESP_NOW_MIDI.setHandleAfterTouchPoly(onPolyAfterTouch);
+
+  //register at the dongle, by sending a message
+  ESP_NOW_MIDI.sendControlChange(127, 127, 16);
+}
+
+void loop() {
+}
