d4b2690d1a828295c54024a44d0d9b4bae5bb697
Thomas Geissl <thomas.geissl@gmail.com>
2025-08-28 10:54:23 +0200

    bumped patch version, forgot to adjust library.properties in the last release

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #define ESP_NOW_MIDI_VERSION_MAJOR 0
 #define ESP_NOW_MIDI_VERSION_MINOR 6
-#define ESP_NOW_MIDI_VERSION_PATCH 0
+#define ESP_NOW_MIDI_VERSION_PATCH 1
 
 inline String getVersion() {
     return String(ESP_NOW_MIDI_VERSION_MAJOR) + "." + 
